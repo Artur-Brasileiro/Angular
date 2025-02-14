ce496ccc09d72c9f1c5644cb21fc586b90b7f7d3
Artur Morais Brasileiro <arturbrasileiro00@gmail.com>
2025-02-13 21:40:09 -0300

    Avanço TypeScript
    Avanço no estudo preparatório para o Angular

--- Inicio TypeScript/build/index.js
+++ Inicio TypeScript/build/index.js
@@ -1,9 +1,43 @@
 "use strict";
-function printaObjeto(pessoa) {
-    console.log(pessoa);
+// VARIAVEIS
+// TIPOS PRIMITIVOS.
+// boolean , number, string.
+let ligado = false;
+let nome = "Artur";
+let idade = 30; // também serve para números com casas decimais.
+// TIPOS ESPECIAIS.
+// null, undefined.
+let nulo = null;
+let indefinido = undefined;
+// TIPOS ABRANGENTES.
+// any, void.
+let retorno;
+let retornoView = "Artur"; // aceita qualquer tipo primitivo.
+// OBJETO - SEM PREVISIBILIDADE.
+let produto = {
+    name: "Artur",
+    cidade: "mg",
+    idade: 30
+};
+let meuProduto = {
+    nome: "Tênis",
+    preco: 89.99,
+    unidades: 54
+};
+// ARRAYS.
+let dados = ["Artur", "Maria", "Felipe"];
+let dados2 = ["Artur", "Maria", "Felipe"];
+// ARRAY DE VARIOS TIPOS.
+let infos = ["Artur", 20, "Maria", 24];
+// TUPLAS.
+let boleto = ["conta de agua", 199.90, 1294124];
+// DATAS.
+let aniversario = new Date("2025-02-13 05:00");
+console.log(aniversario.toString());
+// FUNÇÕES.
+function addNumber(x, y) {
+    return x + y;
 }
-printaObjeto({
-    name: "bruce wayne",
-    vulgo: "batman"
-});
-console.log("NOS DAMOS O CU PARA O BOLSONARO ACEITA QUE DOI MENOS PT");
+;
+let soma = addNumber(3, 5);
+console.log(soma);
--- Inicio TypeScript/src/index.ts
+++ Inicio TypeScript/src/index.ts
@@ -1,15 +1,120 @@
-type heroi ={
+// VARIAVEIS
+
+// TIPOS PRIMITIVOS.
+// boolean , number, string.
+let ligado:boolean = false;
+let nome:string = "Artur";
+let idade:number = 30; // também serve para números com casas decimais.
+
+// TIPOS ESPECIAIS.
+// null, undefined.
+let nulo:null = null;
+let indefinido:undefined = undefined;
+
+// TIPOS ABRANGENTES.
+// any, void.
+let retorno:void
+let retornoView:any = "Artur" // aceita qualquer tipo primitivo.
+
+// OBJETO - SEM PREVISIBILIDADE.
+let produto:object = {
+    name:"Artur",
+    cidade:"mg",
+    idade: 30
+};
+
+// OBJETO TIPADO - COM PREVISIBILIDADE.
+type ProdutoLoja = {
+    nome: string;
+    preco: number;
+    unidades: number;
+}
+
+let meuProduto:ProdutoLoja = {
+    nome: "Tênis",
+    preco: 89.99,
+    unidades: 54
+};
+
+// ARRAYS.
+let dados:string[] = ["Artur", "Maria", "Felipe"];
+let dados2: Array<string> = ["Artur", "Maria", "Felipe"];
+
+// ARRAY DE VARIOS TIPOS.
+let infos:(string | number)[] = ["Artur", 20, "Maria", 24];
+
+// TUPLAS.
+let boleto:[string, number, number] = ["conta de agua", 199.90, 1294124];
+
+// DATAS.
+let aniversario:Date = new Date("2025-02-13 05:00");
+console.log(aniversario.toString());
+
+// FUNÇÕES.
+function addNumber(x:number, y:number):number {
+    return x + y;
+};
+
+let soma:number = addNumber(3, 5);
+console.log(soma);
+
+// FUNÇÕES MULTI TIPOS.
+function callToPhone(phone:number | string) {
+    return phone;
+}
+
+// FUNÇÕES ASSÍNCRONAS.
+async function getDatabase(id:number):Promise<string> {
+    return "Artur";
+}
+
+// INTERFACES.
+interface robot {
+    readonly id: number | string;
     name: string;
-    vulgo: string;
 }
 
-function printaObjeto(pessoa: heroi) {
-    console.log(pessoa);
+const bot: robot = {
+    id: 1,
+    name: "Robocop"
 }
 
-printaObjeto({
-    name: "bruce wayne",
-    vulgo: "batman"
-});
+// CLASSES.
+class Character {
+    name?:string;
+    stregth:number;
+    skill:number;
 
-console.log("NOS DAMOS O CU PARA O BOLSONARO ACEITA QUE DOI MENOS PT");
+    constructor(stregth:number, skill:number) {
+        this.stregth = stregth;
+        this.skill = skill;
+    }
+
+    attack():void {
+        console.log(`Attack with ${this.stregth} points.`);
+    }
+};
+
+const p1 = new Character(10, 98);
+p1.attack();
+
+// GENÉRICOS.
+function concatArray<T>(...itens: T[]): T[] {
+    return new Array().concat(...itens);
+}
+
+const numArray = concatArray<number[]>([1, 5], [3]);
+const stgArray = concatArray<string[]>(["Artur", "Maria"], ["Cachorro"]);
+console.log(numArray);
+console.log(stgArray);
+
+// DECORATORS.
+function ExibirNome(target:any) {
+    console.log(target);
+}
+
+@ExibirNome
+class Funcionario {};
+
+@ExibirNome
+class Quincas {};
